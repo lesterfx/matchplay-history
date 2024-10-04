4d41ee6b932ad15fa008c9cbbdf13fe18dc40869
lesterfx <56974117+lesterfx@users.noreply.github.com>
2024-10-04 15:13:07 -0700

    wip

--- history.js
+++ history.js
@@ -259,7 +259,7 @@ function add_active_game(game) {
 	log(game)
 	let arenaId = game.arenaId
 	let name = arena_by_id[arenaId] || arenaId
-	let button = $('<div>').addClass('game box').data('gameId', game.gameId).data('arenaId'. arenaId)
+	let button = $('<div>').addClass('game box').data('gameId', game.gameId).data('arenaId', arenaId)
 	$('<div>').data('arenaId'. game.arenaId).addClass('arena-title').text(name).appendTo(button)
 	$('<ol>').addClass('players').appendTo(button)
 	$('#active-games').append(button)
