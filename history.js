348920617ee0c7bbbd285b8d6ba4a90d4d70b58b
lesterfx <56974117+lesterfx@users.noreply.github.com>
2024-10-08 21:46:17 -0700

    wip

--- history.js
+++ history.js
@@ -233,7 +233,7 @@ async function get_tournament_details(tournament, add_players) {
 async function get_other(id) {
 	let tournament = all_data.tournament[id];
 	active_players = {};
-	data.users = {}
+	all_data.users = {}
 	let tabs = document.querySelector('#active-tournament');
 	tabs.innerHTML = '';
 	tabs.append(title('tournament', tournament.tournamentId, 'h2'));
