8f466f2bf87ad788a561e6d7d825a4100efb443a
lesterfx <56974117+lesterfx@users.noreply.github.com>
2024-10-14 13:51:21 -0700

    wip

--- history.js
+++ history.js
@@ -597,7 +597,7 @@ function update_player_standing(uid, parent) {
 	let bar = parent.querySelector(`.vs-bars[data-uid="${uid}"]`);
 	bar.style.cssText = `--percent: ${percent}%`;
 	bar.classList.add('ready');
-	parent.querySelector(`.vs-text[data-uid="${uid}"]`).innerHTML = `${won} &mdash; ${lost} vs `;
+	parent.querySelector(`.vs-text[data-uid="${uid}"]`)?.innerHTML = `${won} &mdash; ${lost} vs `;
 }
 function add_player_game(options) {
 	let box = game_element(options.game, false, true, options.won);
