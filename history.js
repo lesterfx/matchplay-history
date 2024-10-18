6c41bcd5c237a989eeeaae2352dad47ebc6f8ba0
lesterfx <56974117+lesterfx@users.noreply.github.com>
2024-10-17 21:06:26 -0700

    wip

--- history.js
+++ history.js
@@ -395,7 +395,7 @@ function arc(factor) {
 	let bx = centerx + radius1*Math.sin(a)
 	let by = centery - radius1*Math.cos(a)
 	let long = (factor > .5) ? 1 : 0
-	let svg = `<svg width="200" height="200"><circle cx="${centerx}" cy=<${centery}" r="${radius1}" stroke="#333333" stroke=width="10px" /><path d="M ${centerx} ${centery} L ${ax} ${ay} A 100 100 0 ${long} 1 ${bx} ${by}" stroke="#fa3838" stroke=width="10px" fill="none" /></svg>`
+	let svg = `<svg width="200" height="200"><circle cx="${centerx}" cy="${centery}" r="${radius1}" stroke="#333333" stroke=width="10px" /><path d="M ${centerx} ${centery} L ${ax} ${ay} A 100 100 0 ${long} 1 ${bx} ${by}" stroke="#fa3838" stroke=width="10px" fill="none" /></svg>`
 	return svg
 }
 async function get_frenzy_position(tournament) {
